f1731842a7c205c43726afae6f44e796aae7eae3
Darshan Baral <darshanbaral@gmail.com>
2019-03-12 19:24:47 -0700

    Removed console.log from toggleTheme.js

--- static/js/toggleTheme.js
+++ static/js/toggleTheme.js
@@ -2,7 +2,6 @@ let toggleTheme = function() {
   if (isDark) {
     for (let key in darkTheme) {
       let elems = document.querySelectorAll(`.${darkTheme[key]}`);
-      console.log(elems);
       elems.forEach(elem => elem.classList.add(lightTheme[key]));
       elems.forEach(elem => elem.classList.remove(darkTheme[key]));
       themeColor.content = "#fff"
@@ -11,7 +10,6 @@ let toggleTheme = function() {
   } else {
     for (let key in darkTheme) {
       let elems = document.querySelectorAll(`.${lightTheme[key]}`);
-      console.log(elems);
       elems.forEach(elem => elem.classList.add(darkTheme[key]));
       elems.forEach(elem => elem.classList.remove(lightTheme[key]));
       themeColor.content = "#000000"
