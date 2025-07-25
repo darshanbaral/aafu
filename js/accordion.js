ab084298ed008720dffdf4250a2938c6f435ec38
darshanbaral <darshanbaral@users.noreply.github.com>
2025-07-25 16:40:06 +0000

    deploy: d793c0a5f620c0faab9f597d6ec98f814732be82

---
+++ js/accordion.js
@@ -0,0 +1,26 @@
+const allPanels = Array.from(document.querySelectorAll(".panel"));
+const allAccordion = Array.from(document.querySelectorAll(".accordion"));
+const expandAccordion = (elem) => {
+  if (!elem.parentElement.classList.contains("active")) {
+    allAccordion.forEach((acc) => {
+      acc.classList.remove("active");
+    });
+    elem.parentElement.classList.add("active");
+    allPanels.forEach(function (elem) {
+      elem.style.maxHeight = null;
+    });
+    let activePanel = elem.parentElement.nextElementSibling;
+    if (
+      activePanel.id != "skill-panel" &&
+      document.querySelector("#skill-panel")
+    ) {
+      let skillBars = Array.from(document.querySelectorAll("#skill-percent"));
+      skillBars.forEach((elem) => {
+        elem.style.width = "0";
+      });
+    }
+    activePanel.style.maxHeight = activePanel.scrollHeight + "px";
+  }
+};
+
+
